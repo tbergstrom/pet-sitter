1fd18ea983f0aa7330f7e2bbdcc86e2a5b6b417e
chandler.carne <chandler.carne@outlook.com>
2023-08-15 14:05:41 -0700

    home, navbar, pettable

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,109 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
+import Nav from './components/Nav';
+import Login from './components/Login';
+import Home from './components/Home';
+import VisitTable from './components/VisitTable';
+import VisitForm from './components/VisitForm';
+import SitterTable from './components/SitterTable';
+import ManageOwnerVisits from './components/ManageOwnerVisits';
+import ManagePets from './components/ManagePets';
+
+import jwtDecode from 'jwt-decode';
+import AuthContext from './contexts/AuthContext';
+import CreateAccount from './components/CreateAccount';
+
 
 function App() {
+  
+  const [sitters, setSitters] = useState([]);
+  const [visits, setVisits] = useState([]);
+  const [user, setUser] = useState(null);
+
+  const login = (token) => {
+    // Decode the token
+    const { sub: username, authorities: authoritiesString } = jwtDecode(token);
+  
+    // Split the authorities string into an array of roles
+    const roles = authoritiesString.split(',');
+  
+    // Create the "user" object
+    const user = {
+      username,
+      roles,
+      token,
+      hasRole(role) {
+        return this.roles.includes(role);
+      }
+    };
+    localStorage.setItem("auth-token", token)
+    setUser(user);
+    return user;
+  };  
+
+  const logout = () => {
+    setUser(null)
+    localStorage.removeItem("auth-token")
+  }
+
+  const auth = {
+    login,
+    logout,
+    user
+  }
+
+  const loadVisits = () => {
+    fetch("http://localhost:8080/api/visit")
+    .then(response => response.json())
+    .then(payload => setVisits(payload))
+  }
+
+  useEffect(loadVisits, [])
+
+  const loadSitters = () => {
+    fetch("http://localhost:8080/api/sitter")
+    .then(response => response.json())
+    .then(payload => setSitters(payload))
+  }
+
+  useEffect(loadSitters, [])
+
+  useEffect(() => {
+    const token = localStorage.getItem("auth-token")
+    if (token) {
+      login(token)
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <AuthContext.Provider value={auth}>
+      <BrowserRouter>
+        <Nav />
+
+        <Routes>
+          {/* always visible */}
+          <Route path='/' element={<Home />}/>
+          <Route path="/findsitter" element={<SitterTable sitters={sitters} loadSitters={loadSitters}/>}/>
+          <Route path='*' element={<p>Page Not Found</p>} />
+
+          {/* logged in as owner and sitter */}
+          <Route path="/visittable/:id" element={ user ? <VisitTable visits={visits} loadVisits={loadVisits} /> : <Navigate to="/" /> } />
+          <Route path="/visittable/visitdetails/:id" element={ user ? <VisitTable visits={visits} loadVisits={loadVisits} /> : <Navigate to="/" /> } />
+          <Route path="/managepets" element={ user ? <ManagePets /> : <Navigate to="/" />} />
+          <Route path="/manageownervisits" element={ user ? <ManageOwnerVisits /> : <Navigate to="/" />} />
+
+          {/* logged in as owner only */}
+          <Route path='/requestvisit' element={ user ? <VisitForm loadVisits={loadVisits}/> : <Navigate to="/" /> }/>
+
+          {/* logged out only */}
+          <Route path='/login' element={ user ? <Navigate to="/" /> : <Login /> }/>
+          <Route path='/create_account' element={ user ? <Navigate to="/" /> : <CreateAccount /> }/>
+        </Routes>
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 }
 
--- client/src/components/PetTable.js
+++ client/src/components/PetTable.js
@@ -1,5 +1,25 @@
 const PetTable = ()=> {
-
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Species</th>
+                    <th>Notes</th>
+                    <th>&nbsp;</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.pets.map(pet => (
+                    <tr key={pet.petId}>
+                        <td>{pet.name}</td>
+                        <td>{pet.petType}</td>
+                        <td>{pet.notes}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    )
 }
 
 export default PetTable;
