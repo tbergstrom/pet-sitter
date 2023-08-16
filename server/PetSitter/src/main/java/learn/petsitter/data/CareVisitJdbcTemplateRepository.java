ecd1b2c02d479735a760185b592440434c1ae7a0
shanwils <64047685+shanwils@users.noreply.github.com>
2023-08-16 11:52:44 -0500

    adding repo layers and partial pet service and controller

--- server/PetSitter/src/main/java/learn/petsitter/data/CareVisitJdbcTemplateRepository.java
+++ server/PetSitter/src/main/java/learn/petsitter/data/CareVisitJdbcTemplateRepository.java
@@ -1,4 +1,39 @@
 package learn.petsitter.data;
 
-public class CareVisitJdbcTemplateRepository {
+import learn.petsitter.models.CareVisit;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class CareVisitJdbcTemplateRepository implements CareVisitRepository{
+    @Override
+    public List<CareVisit> findByOwnerId(int ownerId) {
+        return null;
+    }
+
+    @Override
+    public List<CareVisit> findBySitterId(int sitterId) {
+        return null;
+    }
+
+    @Override
+    public CareVisit findById(int id) {
+        return null;
+    }
+
+    @Override
+    public CareVisit create(CareVisit careVisit) {
+        return null;
+    }
+
+    @Override
+    public boolean update(CareVisit careVisit) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteById(int id) {
+        return false;
+    }
 }
--- server/PetSitter/src/main/java/learn/petsitter/data/ContactInfoJdbcTemplateRepository.java
+++ server/PetSitter/src/main/java/learn/petsitter/data/ContactInfoJdbcTemplateRepository.java
@@ -1,7 +1,32 @@
 package learn.petsitter.data;
 
+import learn.petsitter.models.ContactInfo;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ContactInfoJdbcTemplateRepository {
+public class ContactInfoJdbcTemplateRepository implements ContactInfoRepository{
+    @Override
+    public ContactInfo findById(int id) {
+        return null;
+    }
+
+    @Override
+    public ContactInfo findByAppUserId(int appUserId) {
+        return null;
+    }
+
+    @Override
+    public ContactInfo create(ContactInfo contactInfo) {
+        return null;
+    }
+
+    @Override
+    public boolean update(ContactInfo contactInfo) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteById(int id) {
+        return false;
+    }
 }
