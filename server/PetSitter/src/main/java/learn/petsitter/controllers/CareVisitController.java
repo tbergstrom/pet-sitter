1436e636d33b00c8a0b0edb9f3c7f02464c8c281
shanwils <64047685+shanwils@users.noreply.github.com>
2023-08-16 22:51:32 -0500

    adding care visit repo, server, and controller

--- server/PetSitter/src/main/java/learn/petsitter/controllers/CareVisitController.java
+++ server/PetSitter/src/main/java/learn/petsitter/controllers/CareVisitController.java
@@ -1,4 +1,97 @@
 package learn.petsitter.controllers;
 
+import learn.petsitter.domain.AppUserService;
+import learn.petsitter.domain.CareVisitService;
+import learn.petsitter.domain.Result;
+import learn.petsitter.domain.ResultType;
+import learn.petsitter.models.AppUser;
+import learn.petsitter.models.CareVisit;
+import learn.petsitter.models.Pet;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/visit")
+@CrossOrigin
 public class CareVisitController {
+    private final CareVisitService service;
+    private final AppUserService appUserService;
+
+    public CareVisitController(CareVisitService service, AppUserService appUserService) {
+        this.service = service;
+        this.appUserService = appUserService;
+    }
+
+    @GetMapping("/owner/{ownerId}")
+    public List<CareVisit> findByOwnerId(@PathVariable int ownerId) {
+        return service.findByOwnerId(ownerId);
+    }
+
+    @GetMapping("/sitter/{sitterId}")
+    public List<CareVisit> findBySitterrId(@PathVariable int sitterId) {
+        return service.findBySitterId(sitterId);
+    }
+
+    //TODO think up endpoint for single visit ("/visit/*") ??
+    @GetMapping("/visit/{careVisitId}")
+    public ResponseEntity<CareVisit> findById(@PathVariable int careVisitId) {
+        CareVisit cv = service.findById(careVisitId);
+        if (cv == null) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(cv, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> create(@RequestBody CareVisit cv) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
+        AppUser appUser = (AppUser) appUserService.loadUserByUsername(username);
+        cv.setOwnerId(appUser.getAppUserId());
+
+        Result<CareVisit> result = service.create(cv);
+        if (!result.isSuccess()) {
+            return new ResponseEntity<>(result.getErrorMessages(), HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(result.getPayload(), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/visit/{careVisitId}")
+    public ResponseEntity<?> update(@PathVariable int careVisitId, @RequestBody CareVisit cv) {
+        if (careVisitId != cv.getCareVisitId()) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT); //409
+        }
+
+        Result<CareVisit> result = service.update(cv);
+        if (!result.isSuccess()) {
+            if (result.getResultType() == ResultType.NOT_FOUND) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND); //404
+            } else {
+                return new ResponseEntity<>(result.getErrorMessages(), HttpStatus.BAD_REQUEST); //400
+            }
+        }
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT); //204
+    }
+
+    @DeleteMapping("/visit/{careVisitId}")
+    public ResponseEntity<?> delete(@PathVariable int careVisitId, @RequestBody CareVisit cv) {
+        if (careVisitId != cv.getCareVisitId()) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT); //409
+        }
+
+        Result<CareVisit> result = service.delete(cv);
+        if (!result.isSuccess()) {
+            if (result.getResultType() == ResultType.NOT_FOUND) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND); //404
+            } else {
+                return new ResponseEntity<>(result.getErrorMessages(), HttpStatus.BAD_REQUEST); //400
+            }
+        }
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT); //204
+    }
 }
--- server/PetSitter/src/main/java/learn/petsitter/data/mappers/CareVisitMapper.java
+++ server/PetSitter/src/main/java/learn/petsitter/data/mappers/CareVisitMapper.java
@@ -1,4 +1,28 @@
 package learn.petsitter.data.mappers;
 
-public class CareVisitMapper {
+import learn.petsitter.models.CareVisit;
+import learn.petsitter.models.Pet;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CareVisitMapper implements RowMapper<CareVisit> {
+
+    @Override
+    public CareVisit mapRow(ResultSet resultSet, int i) throws SQLException {
+        CareVisit cv = new CareVisit();
+
+        cv.setCareVisitId(resultSet.getInt("care_visit_id"));
+        cv.setStartDate(resultSet.getDate("start_date"));
+        cv.setStartDate(resultSet.getDate("end_date"));
+        cv.setNotes(resultSet.getString("status"));
+        cv.setStartDate(resultSet.getTime("time_of_day"));
+        cv.setNotes(resultSet.getString("notes"));
+        cv.setCost(resultSet.getBigDecimal("cost"));
+        cv.setOwnerId(resultSet.getInt("owner_id"));
+        cv.setSitterId(resultSet.getInt("sitter_id"));
+
+        return cv;
+    }
 }
