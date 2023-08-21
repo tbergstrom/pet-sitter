7aefdf5afdf3aeb3b292a9e2613fffcd9e4661ac
shanwils <64047685+shanwils@users.noreply.github.com>
2023-08-21 14:09:35 -0500

    updating junit tests

---
+++ server/PetSitter/src/test/java/learn/petsitter/data/AppUserJdbcTemplateRepositoryTest.java
@@ -0,0 +1,20 @@
+package learn.petsitter.data;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppUserJdbcTemplateRepositoryTest {
+
+    @Test
+    void findByUsername() {
+    }
+
+    @Test
+    void getAllOwners() {
+    }
+
+    @Test
+    void getAllSitters() {
+    }
+}
